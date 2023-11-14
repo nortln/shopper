25f50f996f71c88d8a4daecf4b5a8c2d30f0ce79
nortln <=>
2023-11-14 04:42:52 +0100

    semi_final_10

--- dajeweler/settings.py
+++ dajeweler/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 STATIC_URL = 'base/static/'
 STATIC_ROOT = 'base'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
